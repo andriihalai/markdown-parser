241a6ee59c2b4f0be7d1c6f5a3a8c1e9266a1fbd
Halai Andrii <halaiandriy@gmail.com>
2024-03-06 17:01:57 +0200

    Write main file which allow to use console commands

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const fs = require('node:fs/promises');
+const Renderer = require('./lib/Renderer.js');
+
+async function main() {
+  try {
+    const mdFilePath = process.argv[2];
+    const text = await fs.readFile(mdFilePath, 'utf-8');
+
+    const renderer = new Renderer();
+    const output = renderer.render(text);
+
+    if (process.argv.includes('--out')) {
+      const outputFilepathIndex = process.argv.indexOf('--out') + 1;
+      const outputFilepath = process.argv[outputFilepathIndex];
+
+      await fs.writeFile(outputFilepath, output);
+    } else {
+      console.log(output);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+main();
