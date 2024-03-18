06a228ae89af82c7a824bc96d67ab7c85bf84a6a
Halai Andrii <halaiandriy@gmail.com>
2024-03-18 14:48:23 +0200

    Fix regex in Lexer and add test for Renderer

--- test/Rendered.spec.js
+++ test/Rendered.spec.js
@@ -4,7 +4,25 @@ const Renderer = require('./../lib/Renderer');
 
 test('Parsing markdown into html', () => {
   const renderer = new Renderer();
-  const text = renderer.render('**bold**');
-
   expect(renderer.render('**bold**')).toEqual('<p><b>bold</b></p>\n');
+
+  expect(
+    renderer.render(
+      '**Lorem** Ipsum is _simply dummy_ text `of the printing` and typesetting industry.'
+    )
+  ).toEqual(
+    '<p><b>Lorem</b> Ipsum is <i>simply dummy</i> text <tt>of the printing</tt> and typesetting industry.</p>\n'
+  );
+
+  expect(
+    renderer.render(
+      'paragraph\n\n```\npreformatted **text** here\n```\n\nanother paragraph'
+    )
+  ).toEqual(
+    '<p>paragraph</p>\n<p><pre>\npreformatted **text** here\n</pre></p>\n<p>another paragraph</p>\n'
+  );
+
+  expect(renderer.render('**bold** text\n_italic text_')).toEqual(
+    '<p><b>bold</b> text\n<i>italic text</i></p>\n'
+  );
 });
